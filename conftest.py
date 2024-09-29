6a759240bb6efe5dd8108d8e7957b18d45bb23a1
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-09-29 19:48:23 +0200

    Modifying conftest file

--- conftest.py
+++ conftest.py
@@ -2,9 +2,9 @@ import pytest
 from playwright.sync_api import Playwright
 
 @pytest.fixture
-def set_up(page):
-    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    # page = browser.new_page()
+def set_up(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    page = browser.new_page()
     page.goto("https://qa-practice.netlify.app/")
     page.set_default_timeout(3000)
 
